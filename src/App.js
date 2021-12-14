f86021104d6851b85109b6974e9b1a63c54e8694
crhodesy35 <ctrhodes35@gmail.com>
2021-12-14 12:29:39 -0500

    +  flashcard resizing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect} from 'react';
 import FlashcardList from './FlashcardList';
 import './app.css';
-import axios from axios;
+import axios from 'axios';
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+  useEffect(() => {
+    axios.get('https://opentdb.com/api.php?amount=10&category=22&difficulty=easy&type=multiple')
+    .then(response => {
+      setFlashcards(response.data.results.map((questionItem, index) => {
+        const answer = decodeText(questionItem.correct_answer);
+        const options = [...questionItem.incorrect_answers.map(item => decodeText(item)), answer];
+        return {
+          id: `${index}-${Date.now()}`,
+          question: decodeText(questionItem.question),
+          answer,
+          options: options.sort(() => Math.random() - .5)
+        }
+      }));
+      console.log(response.data)
+    })
+  }, [])
+
+  function decodeText(str) {
+    const text = document.createElement('textarea')
+    text.innerHTML = str
+    return text.value
+  }
   return (
+    <div className='container'>
     <FlashcardList flashcards={flashcards}/>
+    </div>
   );
 }
+
 
 const SAMPLE_FLASHCARDS = [
   {
